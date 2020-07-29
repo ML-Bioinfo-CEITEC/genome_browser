70a6c595fcf88ca8197c4165a77897433268a9d4
davidcechak <cechyy@gmail.com>
2020-07-29 14:48:03 +0200

    set up db, update requirements

---
+++ api/db/import.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from database import engine
+
+csv_file_path = 'D:/pekarna/project-bakery/api/db/dummy_protein_df.csv'
+with open(csv_file_path, 'r') as file:
+    data_df = pd.read_csv(file)
+data_df.to_sql('proteins', index = False, con=engine, if_exists='append')
+
+csv_file_path = 'D:/pekarna/project-bakery/api/db/dummy_binding_sites_df.csv'
+with open(csv_file_path, 'r') as file:
+    data_df = pd.read_csv(file)
+data_df.to_sql('binding_sites', index = False, con=engine, if_exists='append')
+
+csv_file_path = 'D:/pekarna/project-bakery/api/db/human_genes_ensrelease99.csv'
+with open(csv_file_path, 'r') as file:
+    data_df = pd.read_csv(file)
+data_df.to_sql('genes', index = False, con=engine, if_exists='append')
---
+++ api/db/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+from database import Base, engine
+
+class Protein(Base):
+    __tablename__ = "proteins"
+    
+    protein_name = Column(String, index=True)
+    protein_id = Column(String, primary_key=True, index=True)
+    uniprot_url = Column(String, index=True)
+
+    # bindings = relationship("BindingSite", back_populates="protein_name")
+
+
+class Gene(Base):
+    __tablename__ = "genes"
+
+    id = Column(String, primary_key=True, index=True)
+    # symbol = Column(String, ForeignKey("proteins.protein_name"), index=True)
+    symbol = Column(String, index=True)
+    biotype = Column(String, index=True)
+    chr = Column(String, index=True)
+    start = Column(Integer, index=True)
+    end = Column(Integer, index=True)
+    strand = Column(String, index=True)
+
+    # tento relationship bude asi trochu tricky
+    # na zacatku set up-nout DB bez nej
+    # je vubec treba?
+
+    # binding_sites = relationship("BindingSite")
+    # binding_sites = relationship('BindingSite', primaryjoin='foreign(genes.symbol) == bindings_site.protein_name')
+
+
+class BindingSite(Base):
+    __tablename__ = "binding_sites"
+
+    id = Column(Integer, primary_key=True, index=True)
+    protein_name = Column(String, index=True)
+    # protein_name = Column(String, ForeignKey("proteins.protein_name"), index=True)
+    chr = Column(String, index=True)
+    start = Column(Integer, index=True)
+    end = Column(Integer, index=True)
+    strand = Column(String, index=True)
+    score = Column(Float, index=True)
+    note = Column(String)
+
+Base.metadata.create_all(bind=engine)
