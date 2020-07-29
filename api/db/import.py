4e9baecc5b69a373626c3a71e70ecd2320405daf
unknown <martinekvlastimil95@gmail.com>
2020-07-29 15:06:46 +0200

    Add base path to import

--- api/db/import.py
+++ api/db/import.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from database import engine
 
-csv_file_path = 'D:/pekarna/project-bakery/api/db/dummy_protein_df.csv'
+base_path = 'C:/Users/Vlastimil Martinek/Documents/repos/projectBakery/api/db'
+
+csv_file_path = f'{base_path}/dummy_protein_df.csv'
 with open(csv_file_path, 'r') as file:
     data_df = pd.read_csv(file)
 data_df.to_sql('proteins', index = False, con=engine, if_exists='append')
 
-csv_file_path = 'D:/pekarna/project-bakery/api/db/dummy_binding_sites_df.csv'
+csv_file_path = f'{base_path}/dummy_binding_sites_df.csv'
 with open(csv_file_path, 'r') as file:
     data_df = pd.read_csv(file)
 data_df.to_sql('binding_sites', index = False, con=engine, if_exists='append')
 
-csv_file_path = 'D:/pekarna/project-bakery/api/db/human_genes_ensrelease99.csv'
+csv_file_path = f'{base_path}/human_genes_ensrelease99.csv'
 with open(csv_file_path, 'r') as file:
     data_df = pd.read_csv(file)
 data_df.to_sql('genes', index = False, con=engine, if_exists='append')
