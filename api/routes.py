bf18d1b71a2f2ce157199cdf5ec521033556b8cc
unknown <martinekvlastimil95@gmail.com>
2020-08-11 08:28:45 +0200

    Add searching endpoint

--- api/routes.py
+++ api/routes.py
@@ -105,14 +105,62 @@ def join():
    return render_template('test.html', rows=results_dicts_array, page=page, pages=pagination.pages)
 
 @genomic.route('/search', methods=["GET"])
+#TODO what if db is not running -> crashes
+#TODO empty result - crashes
 def search():
-   #TODO
-   #search by
    #protein
-   #genomic location
+   protein = request.args.get('protein_name', type=str)
+   #genomic location (genes start/end)
+   loc_min = request.args.get('loc_min', type=int)
+   loc_max = request.args.get('loc_max', type=int)
    #chromozom
-   #gene area
+   chromozom = request.args.get('chr', type=str)
+   #gene area (binding site start/end ?)
+   area_min = request.args.get('area_min', type=int)
+   area_max = request.args.get('area_max', type=int)
+   #score ?
+   score_min = request.args.get('score_min', type=float)
+   score_max = request.args.get('score_max', type=float)
 
+   #control elements
    page = request.args.get('page', type=int, default = 1)
-   # print(page)
-   return jsonify(page)
+   sortby = request.args.get('sort_by', type=str, default="protein_name")
+
+   #building the filters from parameters
+   filters = []
+   if(protein!=None): filters.append(ProteinModel.protein_name == protein)
+   
+   if(loc_min!=None): filters.append(GeneModel.start >= loc_min)
+   if(loc_max!=None): filters.append(GeneModel.end <= loc_max)
+
+   if(chromozom!=None): filters.append(BindingSiteModel.chr == chromozom)
+   if(area_min!=None): filters.append(BindingSiteModel.start >= area_min)
+   if(area_max!=None): filters.append(BindingSiteModel.end <= area_max)
+   if(score_min!=None): filters.append(BindingSiteModel.score >= score_min)
+   if(score_max!=None): filters.append(BindingSiteModel.score <= score_max)
+
+   #bulding the query
+   #Legacy query - querying all models, not only binding site
+   #TODO when i query(ProteinModel) only, i get only 1 on page and multiple results, wtf?
+   #TODO order joins properly
+   # query = db.session.query(ProteinModel, BindingSiteModel, GeneModel)
+   # query = query.join(BindingSiteModel, ProteinModel.protein_name == BindingSiteModel.protein_name)
+   # query = query.join(GeneModel, GeneModel.chr == BindingSiteModel.chr)
+   # query = query.filter(*filters)
+
+   query = BindingSiteModel.query.join(ProteinModel, ProteinModel.protein_name == BindingSiteModel.protein_name)
+   query = query.join(GeneModel, GeneModel.symbol == BindingSiteModel.protein_name)
+   # query = query.distinct(BindingSiteModel.id)
+   query = query.filter(*filters)
+
+   #sorting
+   #.desc() is redundant?
+   if(sortby == 'score'): query = query.order_by(BindingSiteModel.score.desc())
+   if(sortby == "protein_name"): query = query.order_by(BindingSiteModel.protein_name.desc())
+
+   # print(query)
+
+   pagination = query.paginate(page=page, per_page = 25)
+   print(len(pagination.items))
+   serialized = [log.serialize() for log in pagination.items]
+   return render_template('test.html', rows=serialized, page = page, pages = pagination.pages)
