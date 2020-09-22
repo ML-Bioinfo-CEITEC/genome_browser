78881d97445dfc837cabb42d05fab9e99b16fab6
unknown <martinekvlastimil95@gmail.com>
2020-09-22 14:41:11 +0200

    Move secrets

---
+++ api/db/config.py
@@ -0,0 +1,22 @@
+from sqlalchemy.engine import url
+from secrets import SECRET_KEY, POSTGRES
+
+SQLALCHEMY_DATABASE_URL = url.URL(
+        drivername="postgres",
+        username=POSTGRES['user'], 
+        password=POSTGRES['pw'],  
+        host=POSTGRES['host'],  
+        port=POSTGRES['port'], 
+        database=POSTGRES['db'] 
+    )
+    
+SQLALCHEMY_DATABASE_URL_PUBLIC = url.URL(
+        drivername="postgres",
+        username=POSTGRES['user'], 
+        password=POSTGRES['pw'],  
+        host=POSTGRES['public host'],  
+        port=POSTGRES['port'], 
+        database=POSTGRES['db'] 
+    )
+
+
