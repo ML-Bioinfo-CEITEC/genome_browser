c60f85df0499e9620b8e33cc89b6274a61388ad4
unknown <martinekvlastimil95@gmail.com>
2020-09-23 19:51:18 +0200

    Fix inclusive ranges

--- api/db/import_data_utils.py
+++ api/db/import_data_utils.py
@@ -11,7 +11,7 @@ base_path = Path(__file__).parent/"csv_files"
 def create_prejoin(engine):
     session = orm.scoped_session(orm.sessionmaker())(bind=engine)
 
-    sql = "INSERT INTO prejoin SELECT binding_sites.id AS bs_id, binding_sites.protein_name AS protein_name, binding_sites.chr AS chr, binding_sites.start AS bs_start, binding_sites.end AS bs_end, binding_sites.strand AS strand, binding_sites.score AS score, binding_sites.note AS note, genes.id AS gene_id, genes.symbol AS symbol, genes.start AS gene_start, genes.end AS gene_end FROM binding_sites JOIN genes ON genes.strand = binding_sites.strand AND genes.chr = binding_sites.chr AND binding_sites.end > genes.start AND binding_sites.start < genes.end"
+    sql = "INSERT INTO prejoin SELECT binding_sites.id AS bs_id, binding_sites.protein_name AS protein_name, binding_sites.chr AS chr, binding_sites.start AS bs_start, binding_sites.end AS bs_end, binding_sites.strand AS strand, binding_sites.score AS score, binding_sites.note AS note, genes.id AS gene_id, genes.symbol AS symbol, genes.start AS gene_start, genes.end AS gene_end FROM binding_sites JOIN genes ON genes.strand = binding_sites.strand AND genes.chr = binding_sites.chr AND binding_sites.end >= genes.start AND binding_sites.start <= genes.end"
     try:
         session.execute(sql)
         session.commit()
