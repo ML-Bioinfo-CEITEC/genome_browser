9bb1c9001bb170701e2ba1a6510098be95bebcd7
davidcechak <cechyy@gmail.com>
2020-09-25 15:58:16 +0200

    Fix tooltips

---
+++ api/html_content.py
@@ -0,0 +1,9 @@
+tooltips = {
+    "chromosome" : None,
+    "protein_name" : None,
+    "gene_id" : "ensembl gene id",
+    "gene_symbol" : None,
+    "minimum_score" : "minimum score",
+    "binding_location" : None,
+    "gene_location" : None,
+}
